4c5336bca2cdb35e5e5416bab4b38c033f11ebf9
Russell Leggett <russ@taggartsoftware.com>
2016-01-12 19:38:32 -0500

    Added key event listening to use the robot using the remote control

--- app/src/main/java/org/berkshirestech/robotbrain/MainActivity.java
+++ app/src/main/java/org/berkshirestech/robotbrain/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,6 +34,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+    private static String TAG = MainActivity.class.getSimpleName();
 
     private static final int BAUD_RATE = 9600; // BaudRate. Change this value if you need
     public static final int MESSAGE_FROM_SERIAL_PORT = 0;
@@ -40,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
     public static boolean SERVICE_CONNECTED = false;
 
     private UsbSerialPort legPort;
+    private UsbSerialPort headPort;
+    private boolean legMode = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +76,152 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        Log.i(TAG, "key event: " + event);
+        if(event.getRepeatCount() == 0){
+            if(keyCode == KeyEvent.KEYCODE_DPAD_UP){
+                pressUp();
+                return false;
+
+            }else if(keyCode == KeyEvent.KEYCODE_DPAD_LEFT){
+                pressLeft();
+                return false;
+
+            }else if(keyCode == KeyEvent.KEYCODE_DPAD_RIGHT){
+                pressRight();
+                return false;
+
+            }else if(keyCode == KeyEvent.KEYCODE_DPAD_DOWN){
+                pressDown();
+                return false;
+
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+//        Log.i(TAG, "key event: " + event);
+        if(keyCode == KeyEvent.KEYCODE_1){
+            legMode = true;
+        }else if(keyCode == KeyEvent.KEYCODE_2){
+            legMode = false;
+        }else{
+            if(legMode){
+                stopLegs();
+            }else{
+                stopHead();
+            }
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    public void pressUp(){
+        if(legMode){
+            goForward();
+        }else{
+            goUp();
+        }
+    }
+
+    public void pressDown(){
+        if(legMode){
+            goBack();
+        }else{
+            goDown();
+        }
+    }
+
+    public void pressRight(){
+        if(legMode){
+            goRight();
+        }else{
+            goHeadRight();
+        }
+    }
+
+    public void pressLeft(){
+        if(legMode){
+            goLeft();
+        }else{
+            goHeadLeft();
+        }
+    }
+
+    public void goForward(){
+        Log.i(TAG, "go forward");
+        writeLeg("f");
+    }
+
+    public void goBack(){
+        Log.i(TAG, "go back");
+        writeLeg("b");
+    }
+
+    public void goLeft(){
+        Log.i(TAG, "go left");
+        writeLeg("l");
+    }
+
+    public void goRight(){
+        Log.i(TAG, "go right");
+        writeLeg("r");
+    }
+
+    public void stopLegs(){
+        Log.i(TAG, "stopping legs");
+        writeLeg("s");
+    }
+
+    public void stopHead(){
+        Log.i(TAG, "stopping head");
+        writeHead("S");
+    }
+
+    public void goUp(){
+        Log.i(TAG, "head up");
+        writeHead("U");
+    }
+
+    public void goHeadRight(){
+        Log.i(TAG, "head right");
+        writeHead("R");
+    }
+
+    public void goHeadLeft(){
+        Log.i(TAG, "head left");
+        writeHead("L");
+    }
+
+    public void goDown(){
+        Log.i(TAG, "head down");
+        writeHead("D");
+    }
+
+    public void writeLeg(String c){
+        if(legPort != null){
+            try {
+                legPort.write(c.getBytes(), 1000);
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
+    }
+
+
+    public void writeHead(String c){
+        if(headPort != null){
+            try {
+                headPort.write(c.getBytes(), 1000);
+            } catch (IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+        }
     }
 
     @Override
@@ -130,35 +280,33 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        // Read some data! Most have just one port (port 0).
-        UsbSerialPort port = driver.getPorts().get(0);
-        try {
-            port.open(connection);
-            port.setParameters(BAUD_RATE, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
-            byte buffer[] = new byte[8];
+        for(UsbSerialPort port : driver.getPorts()){
 
-            port.write("h".getBytes(), 1000);
+            try {
+                port.open(connection);
+                port.setParameters(BAUD_RATE, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+                byte buffer[] = new byte[8];
 
-            int numBytesRead = port.read(buffer, 1000);
-            if(numBytesRead > 0){
-                char code = (char) buffer[0];
-                if(code == 'l'){
-                    //its the legs
-                    Log.d("USB", "Found the leg port.");
-                    legPort = port;
+                port.write("h".getBytes(), 1000);
+
+                int numBytesRead = port.read(buffer, 1000);
+                if(numBytesRead > 0){
+                    char code = (char) buffer[0];
+                    if(code == 'l'){
+                        //its the legs
+                        Log.d("USB", "Found the leg port.");
+                        legPort = port;
+                    }else if(code == 'h'){
+                        //its the legs
+                        Log.d("USB", "Found the head port.");
+                        headPort = port;
+                    }
                 }
-            }
 
-        } catch (IOException e) {
-            // Deal with error.
-            throw new RuntimeException(e);
-        } finally {
-//            try{
-//                port.close();
-//            } catch (IOException e) {
-//                // Deal with error.
-//                throw new RuntimeException(e);
-//            }
+            } catch (IOException e) {
+                // Deal with error.
+                throw new RuntimeException(e);
+            }
         }
     }
 
